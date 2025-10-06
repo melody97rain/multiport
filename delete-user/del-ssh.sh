626d88adc6bf01f8b95b13a790e6eda34f4e49c9
melody97rain <nilphreakz@outlook.com>
2025-10-06 19:47:07 +0800

    Update del-ssh.sh

--- delete-user/del-ssh.sh
+++ delete-user/del-ssh.sh
@@ -12,29 +12,30 @@ VALIDITY () {
     today=`date -d "0 days" +"%Y-%m-%d"`
     Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
     if [[ $today < $Exp1 ]]; then
-    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
+        echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
     else
-    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
-    echo -e "\e[31mPlease renew your ipvps first\e[0m"
-    exit 0
-fi
+        echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
+        echo -e "\e[31mPlease renew your ipvps first\e[0m"
+        exit 0
+    fi
 }
 IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m"
-VALIDITY
+    echo -e "\e[32mPermission Accepted...\e[0m"
+    VALIDITY
 else
-echo -e "\e[31mPermission Denied!\e[0m";
-echo -e "\e[31mPlease buy script first\e[0m"
-exit 0
+    echo -e "\e[31mPermission Denied!\e[0m";
+    echo -e "\e[31mPlease buy script first\e[0m"
+    exit 0
 fi
+
 echo -e "\e[32mloading...\e[0m"
 clear
 read -p "Username SSH to Delete : " Pengguna
 
 if getent passwd $Pengguna > /dev/null 2>&1; then
-        userdel $Pengguna
-        echo -e "User $Pengguna was removed."
+    /usr/sbin/userdel $Pengguna
+    echo -e "User $Pengguna was removed."
 else
-        echo -e "Failure: User $Pengguna Not Exist."
+    echo -e "Failure: User $Pengguna Not Exist."
 fi
